a8fe6ab9cbcf04cce231178ed95b40bf8987d7be
Tyler Garlick <tjgarlick@gmail.com>
2013-04-29 15:28:20 -0400

    Added nuspec files

---
+++ FlitBit.WebApi/FlitBit.WebApi/HttpModules/ContainerScopePerRequestHttpModule.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using FlitBit.IoC;
+
+namespace FlitBit.WebApi.HttpModules
+{
+    public class ContainerScopePerRequestHttpModule : IHttpModule
+    {
+        IContainer _container;
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += (sender, args) => _container = Container.Current.MakeChildContainer(CreationContextOptions.InheritScope);
+            context.EndRequest += (sender, args) => _container.Dispose(); ;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
